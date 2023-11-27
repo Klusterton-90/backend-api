bbce096f3d9d4f08eb30cb8f1561094f559b7737
sapphire <chidinmaakunwa@gmail.com>
2023-11-27 14:02:06 +0100

    Update login.js

--- controllers/authControllers/login.js
+++ controllers/authControllers/login.js
@@ -1,5 +1,8 @@
 const login = async function (req, res) {
-    res.redirect("/");
+    res.status(200).json({
+        status: 200,
+        message: "User logged in successfully"
+    })
 }
 
 export default login;
