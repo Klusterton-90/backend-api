670f35965e8c04a51f8141fe15a26ca7e4aba246
sapphire <chidinmaakunwa@gmail.com>
2023-11-27 18:04:28 +0100

    Feat: Add chart

--- routes/adherenceRoutes.js
+++ routes/adherenceRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import adherenceControllers from "../controllers/adherenceControllers/index.js";
+import statisticsControllers from "../controllers/statisticsControllers/index.js"
 import ensureAuth from "../helpers/ensureAuth.js";
 
 const router = Router();
@@ -7,5 +8,6 @@ const router = Router();
 router.get("/:healthProfId/adherence", ensureAuth, adherenceControllers.getAllAdherence);
 router.get("/get/:userId/adherence", ensureAuth, adherenceControllers.getAdherence);
 router.post("/:userId/addAdherence", ensureAuth, adherenceControllers.addAdherence);
+router.get("/adherence-chart", ensureAuth, statisticsControllers.adherenceChart);
 
 export default router
