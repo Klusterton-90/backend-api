d93a9bd0ce9af0b263c560c8dd49f7cb90cfd485
sapphire <chidinmaakunwa@gmail.com>
2023-12-08 23:08:18 +0100

    Fix: Passport Auth

--- middlewares/passport/index.js
+++ middlewares/passport/index.js
@@ -4,48 +4,69 @@ import HealthProfessional from "../../models/HealthProfessional.js";
 import bcrypt from "bcrypt";
 import { Strategy as LocalStrategy } from "passport-local";
 
-passport.use(new LocalStrategy(async function (username, password, done) {
-  try {
-    const user = await User.findOne({ where: { name: username } });
-    if (!user) {
-      const professional = await HealthProfessional.findOne({ where: { name: username } });
-      const passwordIsValid = (userPassword, enteredPassword) => {
-        return bcrypt.compareSync(enteredPassword, userPassword);
-      };
-  
-      if (!passwordIsValid(professional.password, password)) {
-        return done(null, false, {
-          message: "Incorrect Password",
-        });
-      }
-      return done(null, professional);
-    }
-    //password validation
-    const passwordIsValid = (userPassword, enteredPassword) => {
-      return bcrypt.compareSync(enteredPassword, userPassword);
-    };
+passport.use(
+  new LocalStrategy(async function (username, password, done) {
+    try {
+      const user = await User.findOne({ where: { email: username } });
+      //If its a patient
+      if (user) {
+        //password validation
+        const passwordIsValid = (userPassword, enteredPassword) => {
+          return bcrypt.compareSync(enteredPassword, userPassword);
+        };
 
-    if (!passwordIsValid(user.password, password)) {
-      return done(null, false, {
-        message: "Incorrect Password",
-      });
+        if (!passwordIsValid(user.password, password)) {
+          return done(null, false, {
+            message: "Incorrect Password",
+          });
+        }
+        return done(null, user);
+      }
+      if (!user) {
+        //If its a health professional
+        const professional = await HealthProfessional.findOne({
+          where: { email: username },
+        });
+
+        if (!professional) {
+          return done(null, false, {
+            message: "User identity not valid",
+          });
+        }
+        const passwordIsValid = (userPassword, enteredPassword) => {
+          return bcrypt.compareSync(enteredPassword, userPassword);
+        };
+
+        if (!passwordIsValid(professional.password, password)) {
+          return done(null, false, {
+            message: "Incorrect Password",
+          });
+        }
+        return done(null, professional);
+      }
+    } catch (error) {
+      return done(error);
     }
-    return done(null, user);
-  } catch (error) {
-    return done(error);
-  }
-}));
+  })
+);
 
 passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-  User.findByPk(id).then(function (user) {
-    done(null, user);
-  });
+  //Check if its a patient
+  if (User.findByPk(id)) {
+    //If true
+    User.findByPk(id).then(function (user) {
+      done(null, user);
+    });
+  } else {
+    //If its not a patient then its a health professional
+    HealthProfessional.findByPk(id).then(function (user) {
+      done(null, user);
+    });
+  }
 });
-
-
 
 export default passport;
