d863d26a207b10a1c27b25d12eda916ef97b57d1
Francis Inegbenose <kizzycole10@gmail.com>
2023-12-08 08:53:58 +0100

    added extra checks to passport middleware and  solved some typos

--- controllers/authControllers/login.js
+++ controllers/authControllers/login.js
@@ -1,37 +1,35 @@
 import User from "../../models/User.js";
 
 const login = async function (req, res) {
-    if(req.isAuthenticated()) {
-        const user = await User.findByPk(req.user.id);
-        if (!user){
-            res.status(200).json({
-                status: 200,
-                userType: "Health Professional",
-                message: "User logged in successfully",
-                data: {
-                    id: req.user.id,
-                    name: req.user.name,
-                    email: req.user.email,
-                    phoneNumber: req.user.phoneNumber
-
-                }
-             })
-        } else if (user) {
-            res.status(200).json({
-                status: 200,
-                userTypre: "Patient",
-                message: "User logged in successfully",
-                data: {
-                    id: req.user.id,
-                    name: req.user.name,
-                    email: req.user.email,
-                    phoneNumber: req.user.phoneNumber,
-                    HealthProfessionalId: req.user.HealthProfessionalId
-    
-                }    
-        })
-        }
-}
-}
+  if (req.isAuthenticated()) {
+    const user = await User.findByPk(req.user.id);
+    if (!user) {
+      res.status(200).json({
+        status: 200,
+        userType: "Health Professional",
+        message: "User logged in successfully",
+        data: {
+          id: req.user.id,
+          name: req.user.name,
+          email: req.user.email,
+          phoneNumber: req.user.phoneNumber,
+        },
+      });
+    } else if (user) {
+      res.status(200).json({
+        status: 200,
+        userType: "Patient",
+        message: "User logged in successfully",
+        data: {
+          id: req.user.id,
+          name: req.user.name,
+          email: req.user.email,
+          phoneNumber: req.user.phoneNumber,
+          HealthProfessionalId: req.user.HealthProfessionalId,
+        },
+      });
+    }
+  }
+};
 
 export default login;
